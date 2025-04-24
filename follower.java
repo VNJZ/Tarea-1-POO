585fbbe0a404e268f25e9e160092d15c996eb01e
VNJZ <104514217+VNJZ@users.noreply.github.com>
2025-04-23 22:42:14 -0400

    etapa 1 con detalles
    falta ver bien el tema de los txt, porque creo que se estan sobreescribiendo y duplicando jaja

---
+++ follower.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+
+public class follower extends subscriber{
+    //constructor que inicializa el nombre y topico del seguidor
+    public follower(String name, String topic){
+        super(name, topic); //llamamos al constructor de la clase base (el suscriber)
+    }
+
+    //sobrescribimos el metodo para recibir el mensaje y mostrarlo de una forma especifica
+    @Override
+    public void receiveMessage(String message){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(name.toLowerCase() + ".txt", true))){
+            //se escribe en msg en el archivo
+            writer.write(message);
+            writer.newLine(); //salto de linea
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
